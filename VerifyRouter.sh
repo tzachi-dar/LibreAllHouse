4bf8aefc231cd673a5ef848b50ac513add2b268d
Tzachi Dar <tzachi.dar@gmail.com>
2024-05-10 12:20:42 +0300

    Get the default gateway everytime.

--- VerifyRouter.sh
+++ VerifyRouter.sh
@@ -5,15 +5,20 @@ echo $DEFAULT_GATEWAY
 
 while true
 do
-    ping -c 1 $DEFAULT_GATEWAY
-    RET=$?
-    if [[ $RET -eq 0 ]]
-    then
-       echo "successes"
-       COUNT=0
+    if [ "$DEFAULT_GATEWAY" = "0.0.0.0" ]; then
+         echo "fail because of 0.0.0.0"
+         COUNT=$((COUNT+1))
     else
-       echo fail
-       COUNT=$((COUNT+1))
+        ping -c 1 $DEFAULT_GATEWAY
+        RET=$?
+        if [[ $RET -eq 0 ]]
+        then
+            echo "successes"
+            COUNT=0
+        else
+            echo fail
+            COUNT=$((COUNT+1))
+        fi
     fi
 
     date
@@ -25,6 +30,9 @@ do
        sudo reboot
     fi
     sleep $SLEEP_TIME
+    # Get the default gateway again
+    DEFAULT_GATEWAY=`route -n | grep ^0.0.0.0 | grep wlan0 |awk '{print $2 }'`
+    echo $DEFAULT_GATEWAY
 
 done
 
